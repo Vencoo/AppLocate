da7dedfb0242967f5e4736985048e6fbf2e6d7b9
lin bo <linbo.2014@gmail.com>
2014-08-11 15:40:02 +0800

    add MainViewController

--- AppLocate/ViewControllers/ALBaseViewController.h
+++ AppLocate/ViewControllers/ALBaseViewController.h
@@ -9,5 +9,7 @@
 #import <UIKit/UIKit.h>
 
 @interface ALBaseViewController : UIViewController
-
+{
+    
+}
 @end
---
+++ AppLocate/ViewControllers/ALMainViewController.h
@@ -0,0 +1,13 @@
+//
+//  ALMainViewController.h
+//  AppLocate
+//
+//  Created by Vencoo-Mac1 on 14-8-11.
+//  Copyright (c) 2014年 Vencoo. All rights reserved.
+//
+
+#import "ALBaseViewController.h"
+
+@interface ALMainViewController : ALBaseViewController
+
+@end
